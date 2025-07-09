136eefa1304e29ed35475d86590fff1758f93d97
Mobbie-tech <mobbiemunyuki@gmail.com>
2025-07-09 14:10:33 +0200

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,8 @@
+#Advanced concept + Strings
+
+message = 'Hello, World!'
+
+print(message.strip()) #Remove leading and trailing whitespace
+print(message.lower()) #Convert all characters to lowercase
+print(message.split(',')) #Split the string into a list based o the comma
+
